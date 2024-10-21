3ea5b394cd8dbfa7af0170212ac260ece80b7097
Rahul Sharma <pt.rahulsharma007@gmail.com>
2024-10-21 21:53:18 +0530

    Resolved merge conflicts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db');
+const db = require('./db'); // database
 
 const ExcelJS = require('exceljs');
 const fs = require('fs');
